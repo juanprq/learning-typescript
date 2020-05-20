ff7cea4851cb8aaa7a8371f810a57e601cb26718
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-05-19 20:51:06 -0500

    adds a class example

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
 import './styles.css';
 
+// classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(client: string, details: string, amount: number) {
+    this.client = client;
+    this.details = details;
+    this.amount = amount;
+  }
+
+  format(): string {
+    return `${this.client} owes ${this.amount} for ${this.details}`;
+  }
+}
+
+const invoice1 = new Invoice('Mario', 'work on the mario website', 250);
+const invoice2 = new Invoice('Luigi', 'work on the luigi app', 300);
+console.log(invoice1.format());
+
+// only objects of type Invoice could be added to this array
+let invoices: Invoice[] = [];
+invoices.push(invoice1);
+invoices.push(invoice2);
+
 // if we are sure the the anchor exists, we can add a bang! operator at the end of the expression.
 const anchor = document.querySelector('a')!;
 // if not, the next expression will fail.
